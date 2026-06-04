c332dc8146f081a2ab323cc77a53b58169eda1c0
slicedofbread <oneslicedofbread@gmail.com>
2026-06-04 19:39:53 +0700

    Add string.go

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("My Name is bread")
+	fmt.Println("My Name is bread"[0])
+
+	panjang_hello := len("Hello World")
+	fmt.Println("Panjang String Hello World = ", panjang_hello)
+
+}
