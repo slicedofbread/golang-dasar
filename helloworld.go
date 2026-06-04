32e8aef66c0f58a885380697c029ea6d43a51e5c
slicedofbread <oneslicedofbread@gmail.com>
2026-06-04 13:47:57 +0700

    Add helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// print bawaan golang
+func main() {
+	println("Hello, World!")
+	baca()
+}
+
+// print pakai package fmt
+func baca() {
+	fmt.Println("Hello, indonesia!")
+}
