d7ba161e0de1cca5cf764511db7b848d7b457c50
slicedofbread <oneslicedofbread@gmail.com>
2026-06-04 19:39:53 +0700

    Add variable.go

---
+++ variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string
+	var age int
+	var isMarried bool
+	var favoriteFood string
+	var fruit = "apple"
+	var (
+		firstName = "first bread"
+		lastName  = "last butter"
+	)
+
+	name = "bread"
+	age = 24
+	isMarried = false
+	favoriteFood = "sushi"
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+	fmt.Println(favoriteFood)
+	fmt.Println(fruit)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
