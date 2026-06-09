1a0f0f541b379da3ef4aa91844a9029683cbd611
slicedofbread <oneslicedofbread@gmail.com>
2026-06-09 19:48:09 +0700

    Add pointer_function.go

---
+++ pointer_function.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type address struct {
+	city, province, country string
+}
+
+func ChangeCityToBandung(address *address) {
+	address.city = "bandung"
+}
+
+func main() {
+	alamat := &address{}
+	ChangeCityToBandung(alamat)
+	fmt.Println(alamat) // akan mencetak: {bandung DKI Jakarta Indonesia}
+}
